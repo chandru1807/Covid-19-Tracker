20b9d65a49ac4338881a8f44d1b2a30d32c5b085
chandru <chandru.c72@gmail.com>
2020-05-18 15:28:07 +0530

    map animation changes

--- src/Components/IndiaMap.jsx
+++ src/Components/IndiaMap.jsx
@@ -13,7 +13,8 @@ const initialTooltip = {
 const upArrowSvg = (className) => (<svg className={'up-arrow-style ' + className} height="32px" id="Layer_1" version="1.1" viewBox="0 0 32 32" width="32px"><path d="M28,14H8.8l4.62-4.62C13.814,8.986,14,8.516,14,8c0-0.984-0.813-2-2-2c-0.531,0-0.994,0.193-1.38,0.58l-7.958,7.958  C2.334,14.866,2,15.271,2,16s0.279,1.08,0.646,1.447l7.974,7.973C11.006,25.807,11.469,26,12,26c1.188,0,2-1.016,2-2  c0-0.516-0.186-0.986-0.58-1.38L8.8,18H28c1.104,0,2-0.896,2-2S29.104,14,28,14z"/></svg>)
 
 export const IndiaMap = ({stateData}) => {
-    console.log(India);
+    India.locations[0]['path'] = ''
+    console.log(India.locations);
     
     const [tooltipStyle, setTooltipStyle] = useState(initialTooltip);
     const [hoveredStateData, sethoveredStateData] = useState({});
@@ -48,11 +49,13 @@ export const IndiaMap = ({stateData}) => {
         let id = event.target.id;
         let clientX = event.clientX;
         let clientY = event.clientY;
+        console.log(clientX, (clientX > (window.screen.width / 2))  ? clientX - 75 : clientX + 75);
+        
         tooltipTimeout = setTimeout(() => {
             setTooltipStyle({
             display: 'block',
             top: clientY + 10,
-            left: clientX - 100
+            left: (clientX > (window.screen.width / 2))  ? clientX - 250 : clientX
         });
         sethoveredStateData(stateData[id]);
         }, 650);
